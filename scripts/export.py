d3b5f348a9371d8dd21b8ddc19d5e223e974e188
Marcos Diaz <marcos@inputlabs.io>
2023-06-27 14:13:19 +0300

    Main iteration over collections

--- scripts/export.py
+++ scripts/export.py
@@ -6,21 +6,46 @@ context = bpy.context
 scene = context.scene
 viewlayer = context.view_layer
 
-obs = [o for o in scene.objects if o.type == 'MESH']
-bpy.ops.object.select_all(action='DESELECT')
-root = Path(bpy.path.abspath("//")).parent
-
 class Entry:
-    def __init__(self, name, stl_name, axis_up='Z', axis_forward='Y',
-            collection=False, transformation=None):
-        self.name = name
+    def __init__(
+            self, col_name, stl_name, axis_up='Z', axis_forward='Y', merge=False,
+            multiple=False, transformation=None, tolerance=False):
+        self.col_name = col_name
         self.stl_name = stl_name
         self.axis_up = axis_up
         self.axis_forward = axis_forward
-        self.collection = collection
+        self.merge = merge
+        self.multiple = multiple
         self.transformation = transformation
+        self.tolerance = tolerance
+
+    @staticmethod
+    def find(name):
+        for entry in entries:
+            if entry.col_name == name:
+                return entry
+
+    def process(self):
+        bpy.ops.object.select_all(action='DESELECT')
+        obs = [o for o in scene.objects if o.type == 'MESH']
+        for ob in obs:
+            if not ob.visible_get(): continue
+            viewlayer.objects.active = ob
+            ob.select_set(True)
+            if self.transformation:
+                self.transformation(ob)
+            if not self.merge:
+                if self.multiple:
+                    subentry = Entry.find(ob.name)
+                    subentry.export()
+                else:
+                    self.export()
+                ob.select_set(False)
+        if self.merge:
+            self.export()
 
     def export(self):
+        root = Path(bpy.path.abspath("//")).parent
         path = root / 'stl' / f'{self.stl_name}.stl'
         bpy.ops.export_mesh.stl(
             filepath=str(path),
@@ -29,15 +54,11 @@ class Entry:
             axis_forward=self.axis_forward)
 
 
-def get_entry(name, collection=False):
-    for entry in entries:
-        if entry.name == name and entry.collection == collection:
-            return entry
-
 def rotation(ob, x=0, y=0, z=0):
     if x: ob.rotation_euler.x = math.radians(x)
     if y: ob.rotation_euler.y = math.radians(y)
     if z: ob.rotation_euler.z = math.radians(z)
+
 
 entries = [
     Entry('Case front',       'primary_015mm_front'),
@@ -54,24 +75,14 @@ entries = [
     Entry('Dpad',             'secondary_007mm_dpad'),
     Entry('Thumbstick',       'secondary_007mm_thumbstick_(support)'),
     Entry('Home',             'secondary_007mm_home_(support)', transformation=lambda ob: rotation(ob, -8.4)),
-    Entry('Hexagon R',        'conductive_015mm_hexagon', '-Z'),
+    Entry('Hexagon',          'conductive_015mm_hexagon', '-Z', tolerance=True),
     Entry('Wheel support',    'any_015mm_wheelholder'),
     Entry('Anchor',           'any_015mm_anchors_(brim)', '-Z'),
-    Entry('Soldering helper', 'any_020mm_solderstand', '-Z', collection=True),
+    Entry('Soldering helper', 'any_020mm_solderstand', '-Z', merge=True),
+    Entry('Scrollwheel', None, multiple=True),
 ]
 
 for collection in bpy.data.collections:
-    collection = get_entry(collection.name, True)
-    for ob in obs:
-        if (ob.visible_get()):
-            entry = collection or get_entry(ob.name)
-            if not entry: break
-            viewlayer.objects.active = ob
-            ob.select_set(True)
-            if entry.transformation:
-                entry.transformation(ob)
-            if not collection:
-                entry.export()
-                ob.select_set(False)
-    if collection:
-        collection.export()
+    entry = Entry.find(collection.name)
+    entry.process()
+    break
